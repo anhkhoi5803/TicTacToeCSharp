0c056af935e1095e89e54c955cacde4869ed4212
anhkhoi5803 <anhkhoi5803@gmail.com>
2023-02-11 15:59:00 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace TicTacT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnResetScore = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btn8);
+            this.panel1.Controls.Add(this.btn7);
+            this.panel1.Controls.Add(this.btn6);
+            this.panel1.Controls.Add(this.btn5);
+            this.panel1.Controls.Add(this.btn4);
+            this.panel1.Controls.Add(this.btn3);
+            this.panel1.Controls.Add(this.btn2);
+            this.panel1.Controls.Add(this.btn1);
+            this.panel1.Controls.Add(this.btn0);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(210, 210);
+            this.panel1.TabIndex = 0;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(140, 140);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(70, 70);
+            this.btn8.TabIndex = 9;
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(70, 140);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(70, 70);
+            this.btn7.TabIndex = 8;
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(0, 140);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(70, 70);
+            this.btn6.TabIndex = 7;
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(140, 70);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(70, 70);
+            this.btn5.TabIndex = 6;
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(70, 70);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(70, 70);
+            this.btn4.TabIndex = 5;
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(0, 70);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(70, 70);
+            this.btn3.TabIndex = 4;
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(140, 0);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(70, 70);
+            this.btn2.TabIndex = 3;
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(70, 0);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(70, 70);
+            this.btn1.TabIndex = 2;
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(0, 0);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(70, 70);
+            this.btn0.TabIndex = 1;
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Engravers MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(228, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player X:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Engravers MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(365, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 19);
+            this.label2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Engravers MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(229, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 19);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Player O:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Engravers MT", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(365, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 19);
+            this.label4.TabIndex = 4;
+            // 
+            // btnResetScore
+            // 
+            this.btnResetScore.Location = new System.Drawing.Point(272, 199);
+            this.btnResetScore.Name = "btnResetScore";
+            this.btnResetScore.Size = new System.Drawing.Size(126, 23);
+            this.btnResetScore.TabIndex = 5;
+            this.btnResetScore.Text = "Reset Score:";
+            this.btnResetScore.UseVisualStyleBackColor = true;
+            this.btnResetScore.Click += new System.EventHandler(this.btnResetScore_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 244);
+            this.Controls.Add(this.btnResetScore);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnResetScore;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacT
+{
+    public partial class Form1 : Form
+    {
+        int turn = 0;
+        string player = "X";
+        Button[] buttons = new Button[9];
+        int[] grids = new int[9];
+        int pointX = 0;
+        int pointO = 0;
+        public Form1()
+        {
+            InitializeComponent();
+
+            buttons[0] = btn0;
+            buttons[1] = btn1;
+            buttons[2] = btn2;
+            buttons[3] = btn3;
+            buttons[4] = btn4;
+            buttons[5] = btn5;
+            buttons[6] = btn6;
+            buttons[7] = btn7;
+            buttons[8] = btn8;
+
+
+            for (int i = 0; i < 9; i++)
+            {
+                buttons[i].Click += ButtonClick;
+                buttons[i].Tag = i;
+            }
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            btnResetScore.Enabled = false;
+            Button bt = (Button)sender;
+
+            bt.Enabled = false;
+
+            turn++;
+
+            int gridPlayed = (int)bt.Tag;
+
+            bt.Text = player;
+            grids[gridPlayed] = player.Equals("X") ? 1 : 2;
+
+
+
+
+            if (CheckWinCon())
+            {
+
+                MessageBox.Show("player "+player + " won the game");
+                if (player.Equals("X"))
+                {
+                    pointX += 10;
+                    label2.Text = pointX.ToString();
+                }
+                else
+                {
+                    pointO += 10;
+                    label4.Text = pointO.ToString();
+                }
+
+                ResetGame();
+                return;
+            }
+
+            if (turn == 9)
+            {
+                MessageBox.Show("The game is a tie");
+                ResetGame();
+
+                return;
+            }
+
+
+            player = player.Equals("X") ? "O" : "X";
+        }
+
+
+        private bool CheckWinCon()
+        {
+            if (CheckRow() || CheckCol() || CheckDiag())
+                return true;
+
+            return false;
+        }
+
+        private bool CheckRow()
+        {
+            for (int i = 0; i < 9; i += 3)
+            {
+                if (grids[i] == 0) continue;
+
+                if (grids[i] == grids[i + 1] && grids[i] == grids[i + 2]) return true;
+            }
+            return false;
+        }
+
+        private bool CheckCol()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (grids[i] == 0) continue;
+
+                if (grids[i] == grids[i + 3] && grids[i] == grids[i + 6]) return true;
+            }
+            return false;
+        }
+
+        private bool CheckDiag()
+        {
+            if (grids[4] == 0) return false;
+            if (grids[0] == grids[4] && grids[4] == grids[8]) return true;
+            if (grids[2] == grids[4] && grids[4] == grids[6]) return true;
+
+            return false;
+        }
+
+        void ResetGame()
+        {
+            turn = 0;
+            player = "X";
+            for (int i = 0; i < 9; i++)
+            {
+                buttons[i].Enabled = true;
+                buttons[i].Text = "";
+
+                grids[i] = 0;
+            }
+            btnResetScore.Enabled = true;
+        }
+
+        // Create ai opponent
+        int evaluate()
+        {
+            if(CheckRow()==true) { }
+
+            return 0;
+        }
+        bool isMovesLeft(int[] grid)
+        {
+            for(int i = 0; i< grid.Length; i++)
+            {
+                if (grid[i] == 0) return true;
+            }
+            return false;
+        }
+
+        int miniMax(int[] grid,int depth, bool isMax) 
+        {
+            bool isWon = CheckWinCon();
+            int best = int.MinValue;
+
+            if (isWon) return 10;
+            
+
+            return 1;
+        }
+
+        private void btnResetScore_Click(object sender, EventArgs e)
+        {
+            label2.Text= string.Empty;
+            label4.Text = string.Empty;
+            MessageBox.Show("Score is reseted");
+
+        }
+    }
+}
